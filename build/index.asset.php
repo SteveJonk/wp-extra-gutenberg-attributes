62722cfc3c065b77ae32c4cecaca20e72a0c0327
Steve Jonk <stevejonk1993@gmail.com>
2023-12-28 17:33:17 +0100

    chore: minify build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => 'f3def17d1f8804b10f27');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks', 'wp-i18n'), 'version' => '287d2dc631efcbbfcf99');
